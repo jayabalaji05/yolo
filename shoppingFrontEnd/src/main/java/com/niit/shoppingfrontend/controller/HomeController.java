5a43de20541e8d8519f5ebe55bd5803b6aa7f72c
jayabalaji05 <jb20594@gmail.com>
2016-10-20 18:12:03 +0530

    my second commit

---
+++ shoppingFrontEnd/src/main/java/com/niit/shoppingfrontend/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.niit.shoppingfrontend.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+
+	@RequestMapping("/login")
+public String login(){
+		return "login";
+	}
+		@RequestMapping("/register")
+		public String register()
+		{
+			return "register";
+		}
+}
--- shoppingFrontEnd/src/main/webapp/index.jsp
+++ shoppingFrontEnd/src/main/webapp/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2> <center>Shopping cart</center>	</h2>
+<hr>
+Existing User <a href="login">login here</a><br>
+Register <a href="register">here</a>
+<hr>
 </body>
 </html>
